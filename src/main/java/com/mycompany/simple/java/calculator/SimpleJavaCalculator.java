72836e6343c94cb127722cbf8edb4c3df40af30b
Rajeen Balasuriya <rajeenbalasuriya1@gmail.com>
2023-12-08 19:16:49 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/simple/java/calculator/SimpleJavaCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simple.java.calculator;
+
+/**
+ *
+ * @author rajeen
+ */
+public class SimpleJavaCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
